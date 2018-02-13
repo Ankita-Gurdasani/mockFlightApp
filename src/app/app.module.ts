a17c9ece420ead5db0da7a0f287d153c8032ed36
Thecrustyengineer <thecrustyengineer@gmail.com>
2018-02-13 11:25:17 +0530

    First commit. Routing still doesn't work. Filters work. Error messages in place. Service not using promises.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
